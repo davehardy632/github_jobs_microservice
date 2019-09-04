0e4e86e537c2193077fd7192554077d1e3a2181e
David Hardy <davehardy632@gmail.com>
2019-09-04 13:25:32 -0600

    Adding cors setup.

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ListingsAndCityMicroservice
+module GithubJobsMicroservice
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
@@ -31,7 +31,7 @@ module ListingsAndCityMicroservice
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    
+
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
