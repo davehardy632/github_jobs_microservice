02b7e47876a861d4f655c8ff444975ac648427df
David Hardy <davehardy632@gmail.com>
2019-09-05 21:45:08 -0600

    Created test to return average salary data for a given urban area.

--- spec/requests/api/v1/teleport_city_data_spec.rb
+++ spec/requests/api/v1/teleport_city_data_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Teleport api" do
   it "A user can get data about a given city" do
 
-    get '/api/v1/urban_area_scores', headers: { 'HTTP_LOCATION' => "Austin, TX" }
+    get '/api/v1/urban_area/scores', headers: { 'HTTP_LOCATION' => "Austin, TX" }
 
     expect(response).to be_successful
 
@@ -23,9 +23,13 @@ describe "Teleport api" do
 
     city_info = JSON.parse(response.body)
 
-    city_info_keys = ["teleport_city_score", "summary", "categories"]
+    city_info_keys = ["job", "salary_percentiles"]
+    city_info_job_keys = ["id", "title"]
+    city_info_salary_percentile_keys = ["percentile_25", "percentile_50", "percentile_75"]
 
-    expect(city_info.length).to eq(3)
-    expect(city_info.keys).to eq(city_info_keys)
+    expect(city_info.length).to eq(52)
+    expect(city_info[0].keys).to eq(city_info_keys)
+    expect(city_info[0]["job"].keys).to eq(city_info_job_keys)
+    expect(city_info[0]["salary_percentiles"].keys).to eq(city_info_salary_percentile_keys)
   end
 end
