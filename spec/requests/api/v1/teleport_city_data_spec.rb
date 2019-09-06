d1f447ad3d270177690e041d13ef6dec23614ecf
David Hardy <davehardy632@gmail.com>
2019-09-05 21:09:16 -0600

    Updated test to return urban area scores using lat and long.

--- spec/requests/api/v1/teleport_city_data_spec.rb
+++ spec/requests/api/v1/teleport_city_data_spec.rb
@@ -3,12 +3,29 @@ require 'rails_helper'
 describe "Teleport api" do
   it "A user can get data about a given city" do
 
-    get '/api/v1/city_data', headers: { 'HTTP_LOCATION' => "Denver, CO" }
+    get '/api/v1/urban_area_scores', headers: { 'HTTP_LOCATION' => "Austin, TX" }
 
     expect(response).to be_successful
 
     city_info = JSON.parse(response.body)
-binding.pry
+
+    city_info_keys = ["teleport_city_score", "summary", "categories"]
+
     expect(city_info.length).to eq(3)
+    expect(city_info.keys).to eq(city_info_keys)
+  end
+
+  it "A user can get data about average salary in the nearest urban area" do
+
+    get '/api/v1/urban_area/salaries', headers: { 'HTTP_LOCATION' => "Austin, TX" }
+
+    expect(response).to be_successful
+
+    city_info = JSON.parse(response.body)
+
+    city_info_keys = ["teleport_city_score", "summary", "categories"]
+
+    expect(city_info.length).to eq(3)
+    expect(city_info.keys).to eq(city_info_keys)
   end
 end
