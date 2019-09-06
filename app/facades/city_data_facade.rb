f414417ddfb397ddc9790445849de7f7bdf1dedf
David Hardy <davehardy632@gmail.com>
2019-09-05 18:52:58 -0600

    Updated facade to handle multiple api call aggregation to get the pertinent score data for a city.

--- app/facades/city_data_facade.rb
+++ app/facades/city_data_facade.rb
@@ -12,11 +12,36 @@ class CityDataFacade
     city_data_info[:_embedded][:"city:search-results"][0][:_links][:"city:item"][:href]
   end
 
-  def 
+  def urban_area_link
+    teleport_basic_city_info_service.basic_city_info[:_links][:"city:urban_area"][:href]
+  end
 
-  def quality_of_life_stats
-    teleport_basic_city_info_service.basic_city_info
-    binding.pry
+  def urban_area_scores
+    teleport_city_scores_service.return_scores
+  end
+
+  def score_categories
+    urban_area_scores[:categories]
+  end
+
+  def score_summary
+    urban_area_scores[:summary]
+  end
+
+  def total_avg_score
+    urban_area_scores[:teleport_city_score]
+  end
+
+  def response_obj
+    score_response = {}
+    score_response[:teleport_city_score] = total_avg_score
+    score_response[:summary] = score_summary
+    score_response[:categories] = score_categories
+    score_response
+  end
+
+  def quality_of_life_scores
+    response_obj
   end
 
   private
@@ -29,6 +54,10 @@ class CityDataFacade
 
   def teleport_basic_city_info_service
     TeleportBasicCityInfoService.new(city_info_url)
+  end
+
+  def teleport_city_scores_service
+    TeleportCityScoresService.new(urban_area_link)
   end
 
 end
