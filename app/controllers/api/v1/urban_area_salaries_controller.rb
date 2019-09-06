06a8b1b8ac7d2d873d64454fd6aabde0cae7743b
David Hardy <davehardy632@gmail.com>
2019-09-05 21:46:28 -0600

    Created a controller to handle salary request.

---
+++ app/controllers/api/v1/urban_area_salaries_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::UrbanAreaSalariesController < ApplicationController
+
+  def index
+    if valid_header?
+      render :json => JSON.parse(city_data_facade.average_salaries.to_json()), :status => :ok
+    else
+      invalid_header = {
+        error: "Missing location header",
+        status: 400
+      }
+        render :json => invalid_header, :status => :unauthorized
+      end
+  end
+
+  private
+
+  def valid_header?
+    if request.env["HTTP_LOCATION"] != nil
+      return true
+    else
+      return false
+    end
+  end
+
+  def city_data_facade
+    CityDataFacade.new(request.env["HTTP_LOCATION"])
+  end
+end
