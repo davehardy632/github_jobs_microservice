ea7ee046f6e6d7221daf79ab41a9d11db8607c61
David Hardy <davehardy632@gmail.com>
2019-09-06 17:06:25 -0600

    Updating methods in the city data facade to account for edge cases.

--- app/facades/city_data_facade.rb
+++ app/facades/city_data_facade.rb
@@ -9,43 +9,58 @@ class CityDataFacade
   end
 
   def urban_area_url
-    teleport_location_service.closest_urban_area[:_embedded][:"location:nearest-urban-areas"][0][:_links][:"location:nearest-urban-area"][:href]
+    if teleport_location_service.closest_urban_area[:_embedded][:"location:nearest-urban-areas"] == []
+      teleport_location_service.closest_urban_area[:_embedded][:"location:nearest-cities"][0][:_links][:"location:nearest-city"][:href]
+    else
+      teleport_location_service.closest_urban_area[:_embedded][:"location:nearest-urban-areas"][0][:_links][:"location:nearest-urban-area"][:href]
+    end
   end
 
   def salaries_obj
-    teleport_city_salaries_service.return_salaries
+    no_salary_data = {
+      message: "There is no available salary data for this city."
+    }
+    if teleport_city_salaries_service.return_salaries[:message] == "Not Found"
+      no_salary_data
+    else
+      teleport_city_salaries_service.return_salaries
+    end
   end
 
   def average_salaries
-    salaries_obj[:salaries]
+    if salaries_obj[:salaries]
+      salaries_obj[:salaries]
+    else
+      salaries_obj
+    end
   end
 
-  def urban_area_scores
-    teleport_city_scores_service.return_scores
+  def non_urban_data(response)
+    non_urban_data_response = {}
+    non_urban_data_response[:full_name] = response[:full_name]
+    non_urban_data_response[:population] = response[:population]
+    non_urban_data_response[:status] = "Nearest urban area info is currently unavailable."
+    non_urban_data_response
   end
 
-  def score_categories
-    urban_area_scores[:categories]
-  end
-
-  def score_summary
-    urban_area_scores[:summary]
-  end
-
-  def total_avg_score
-    urban_area_scores[:teleport_city_score]
-  end
-
-  def response_obj
+  def urban_data(response)
     score_response = {}
-    score_response[:teleport_city_score] = total_avg_score
-    score_response[:summary] = score_summary
-    score_response[:categories] = score_categories
+    score_response[:teleport_city_score] = response[:teleport_city_score]
+    score_response[:summary] = response[:summary]
+    score_response[:categories] = response[:categories]
     score_response
   end
 
+  def urban_area_scores
+    if teleport_city_scores_service.return_scores.keys[1] == :categories
+      urban_data(teleport_city_scores_service.return_scores)
+    else
+      non_urban_data(teleport_city_scores_service.return_scores)
+    end
+  end
+
   def quality_of_life_scores
-    response_obj
+    urban_area_scores
   end
 
   private
