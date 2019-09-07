81ca4787f95608bbc047f74dc197bdbeac58a682
David Hardy <davehardy632@gmail.com>
2019-09-06 18:36:18 -0600

    Created a route for city details endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get '/urban_area/scores', to: 'urban_area_scores#index'
       get '/urban_area/salaries', to: 'urban_area_salaries#index'
       get '/urban_area/images', to: 'urban_area_images#index'
+      get '/urban_area/details', to: 'urban_area_details#index'
     end
   end
 end
