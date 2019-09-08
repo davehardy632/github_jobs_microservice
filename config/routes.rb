59d1da1f0c3b3adb390d047c176fec8bec562477
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-08 16:20:16 -0600

    user login with tests and sad path
    co-authored-by: David Hardy<davehardy632@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/users', to: 'users#index'
+      post '/sessions', to: 'sessions#index'
       get '/listings', to: 'listings#index'
       get '/urban_area/scores', to: 'urban_area_scores#index'
       get '/urban_area/salaries', to: 'urban_area_salaries#index'
