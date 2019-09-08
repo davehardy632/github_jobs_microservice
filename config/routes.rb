d48ef7709cff9349a755ac4d748f55d865411cd5
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-08 14:42:03 -0600

    Added controller for user Registration
    co-authored-by: David Hardy <davehardy632@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      post '/users', to: 'users#index'
       get '/listings', to: 'listings#index'
       get '/urban_area/scores', to: 'urban_area_scores#index'
       get '/urban_area/salaries', to: 'urban_area_salaries#index'
