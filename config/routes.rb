87234d8fbd67bd673b2bfea12a21f174f2c38ce5
David Hardy <davehardy632@gmail.com>
2019-09-09 15:33:43 -0600

    Route for making api call to teleport to recieve basic origin city info.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       post '/users', to: 'users#index'
       post '/sessions', to: 'sessions#index'
       get '/listings', to: 'listings#index'
+      get '/city_info', to: 'city_info#index'
       get '/urban_area/scores', to: 'urban_area_scores#index'
       get '/urban_area/salaries', to: 'urban_area_salaries#index'
       get '/urban_area/images', to: 'urban_area_images#index'
