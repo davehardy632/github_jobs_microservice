a23dca498a483c3a57a3c909abe172e416a8f1e6
David Hardy <davehardy632@gmail.com>
2019-09-09 15:33:09 -0600

    Controller made to recieve city info request.

---
+++ app/controllers/api/v1/city_info_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::CityInfoController < ApplicationController
+
+
+  def index
+    if valid_header?
+      render :json => JSON.parse(city_info_facade.return_info.to_json()), :status => :ok
+    else
+      invalid_header = {
+        error: "Missing location header",
+        status: 400
+      }
+        render :json => invalid_header, :status => :unauthorized
+      end
+  end
+
+  private
+
+  def valid_header?
+    if request.env["HTTP_LOCATION"] != nil
+      return true
+    else
+      return false
+    end
+  end
+
+  def city_info_facade
+    CityInfoFacade.new(request.env["HTTP_LOCATION"])
+  end
+
+end
