380ca2a16dc78b94735d3556e554ec4bc1c94792
David Hardy <davehardy632@gmail.com>
2019-09-09 15:32:35 -0600

    Created test to return city info based on location origin.

---
+++ spec/requests/api/v1/origin_city_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Teleport Api return origin city data" do
+  it "When a location header is sent for a city, that citys general info is returned" do
+    get '/api/v1/city_info', headers: { 'HTTP_LOCATION' => "Louisville, CO" }
+
+    expect(response).to be_successful
+
+    city_info = JSON.parse(response.body)
+binding.pry
+    expect(city_info.count).to eq(3)
+  end
+end
