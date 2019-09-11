222326c0b0fc2362b1c23e256fab898e10e997ec
David Hardy <davehardy632@gmail.com>
2019-09-11 10:35:32 -0600

    Updating tests to mock listing data.

--- spec/requests/api/v1/job_listing_request_spec.rb
+++ spec/requests/api/v1/job_listing_request_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 
 describe "Jooble Job listings api" do
+  before :each do
+
+    json_listing_response = File.open("./fixtures/jooble_denver_listing_data.json")
+
+    stub_request(:post, "https://us.jooble.org/api/#{ENV['JOOBLE_API_KEY']}").
+        with(
+          body: "{\"location\":\"Denver, CO\"}",
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'Location'=>'Denver, CO',
+         'User-Agent'=>'Faraday v0.15.4'
+          }).
+        to_return(status: 200, body: json_listing_response, headers: {})
+
+      stub_request(:post, "https://us.jooble.org/api/#{ENV['JOOBLE_API_KEY']}?location=Denver,%20CO").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Faraday v0.15.4'
+          })
+
+  end
   it "Returns Job listings for a location and keyword" do
 
     get '/api/v1/listings', headers: { 'HTTP_LOCATION' => "Denver, CO" }
@@ -8,7 +32,7 @@ describe "Jooble Job listings api" do
     expect(response).to be_successful
 
     listing_data = JSON.parse(response.body)
-
+    
     job_keys = ["title", "location", "snippet", "salary", "source", "type", "link", "company", "updated", "id"]
 
     expect(listing_data["jobs"].count).to eq(20)
